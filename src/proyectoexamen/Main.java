3be7ae7ef3acb6b98b5beb2dfb7680a8bff33246
Jaime Reyes-91 <jreyesr26@miumg.edu.gt>
2025-09-22 07:31:13 -0600

    Primera Versión del Proyecto, entregado como evaluación

---
+++ src/proyectoexamen/Main.java
@@ -0,0 +1,24 @@
+
+package proyectoexamen;
+
+import Conexion.Conexion;
+import Formulario.Cliente;
+import Formulario.Estudiante;
+import Formulario.Login;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        Conexion conexionPostgres = new Conexion();
+        Connection con = conexionPostgres.getConexion();
+    }
+    
+}
